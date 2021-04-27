34f1ad40e350af8c8916218d3e661b3f45198280
mashingan <rahmat.d.ruffy@gmail.com>
2021-04-27 20:52:04 +0700

    add more test for fetch simple string

--- client_test.go
+++ client_test.go
@@ -1,8 +1,19 @@
 package localredis
 
 import (
+	"strings"
 	"testing"
 )
+
+func assertStr(t *testing.T, fetchstr, expected string, pos, expectpos int) {
+	if fetchstr != expected {
+		t.Errorf("Invalid string fetch, expected %s, got %s\n", expected, fetchstr)
+	}
+	if pos != expectpos {
+		t.Errorf("Invalid position, expected %d, got %d\n", expectpos, pos)
+	}
+
+}
 
 func TestFetchSimpleString(t *testing.T) {
 	// simple string cannot hold utf-8 byte codes and will fail
@@ -13,10 +24,11 @@ func TestFetchSimpleString(t *testing.T) {
 	fetchstr, pos, _ := fetchSimpleString(rawbyte)
 	t.Log("fetchstr:", fetchstr)
 	t.Log("pos:", pos)
-	if fetchstr != expected {
-		t.Errorf("Invalid string fetch, expected %s, got %s\n", expected, fetchstr)
-	}
-	if pos != len(rawbyte) {
-		t.Errorf("Invalid position, expected %d, got %d\n", len(rawbyte), pos)
-	}
+	assertStr(t, fetchstr, expected, pos, len(rawbyte))
+
+	expected = "hello    world"
+	rawbyte = []byte("+hello    world\r\nnananan")
+	fetchstr, pos, _ = fetchSimpleString(rawbyte)
+	assertStr(t, fetchstr, expected, pos,
+		strings.Index(string(rawbyte), expected)+len(expected)+2)
 }
