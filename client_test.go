1e062ad3751fbe4d0ae977ef1517febbce2d7027
mashingan <rahmat.d.ruffy@gmail.com>
2021-04-28 14:47:17 +0700

    move out networked test another file

--- client_test.go
+++ client_test.go
@@ -2,11 +2,8 @@ package localredis
 
 import (
 	"fmt"
-	"net"
 	"strings"
-	"sync"
 	"testing"
-	"time"
 )
 
 func assertStr(t *testing.T, fetchstr, expected string, pos, expectpos int) {
@@ -180,46 +177,4 @@ func TestArray(t *testing.T) {
 		t.Errorf("invalid error value, got %v, expected Bar", theerr)
 	}
 
-}
-
-func TestListenAndServe(t *testing.T) {
-	var w sync.WaitGroup
-	w.Add(1)
-	addr := ":9025"
-	// go ListenAndServe(addr)
-	go func(wg *sync.WaitGroup) {
-		defer w.Done()
-		ListenAndServe(addr)
-	}(&w)
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	orig := "hello world"
-	raw := []byte(createSimpleString(orig))
-	n, err := conn.Write(raw)
-	t.Log("sent:", n)
-	if err != nil {
-		t.Error(err)
-	}
-	if n < 1 {
-		t.Errorf("invalid sending, got sent 0, expected %d\n", len(raw))
-	}
-
-	orig = "hello 異世界"
-	raw = []byte(createBulkString(orig))
-	n, err = conn.Write(raw)
-	t.Log("sent:", n)
-	if err != nil {
-		t.Error(err)
-	}
-	if n < 1 {
-		t.Errorf("invalid sending, got sent 0, expected %d\n", len(raw))
-	}
-	time.Sleep(1 * time.Second)
-	Close()
-	w.Wait()
-	// raw = []byte(createArrayRepr([]interface{}{
-	// 	"hello world"
-	// }))
 }
---
+++ networked_test.go
@@ -0,0 +1,50 @@
+package localredis
+
+import (
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestListenAndServe(t *testing.T) {
+	var w sync.WaitGroup
+	w.Add(1)
+	addr := ":9025"
+	// go ListenAndServe(addr)
+	go func(wg *sync.WaitGroup) {
+		defer w.Done()
+		ListenAndServe(addr)
+	}(&w)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	orig := "hello world"
+	raw := []byte(createSimpleString(orig))
+	n, err := conn.Write(raw)
+	t.Log("sent:", n)
+	if err != nil {
+		t.Error(err)
+	}
+	if n < 1 {
+		t.Errorf("invalid sending, got sent 0, expected %d\n", len(raw))
+	}
+
+	orig = "hello 異世界"
+	raw = []byte(createBulkString(orig))
+	n, err = conn.Write(raw)
+	t.Log("sent:", n)
+	if err != nil {
+		t.Error(err)
+	}
+	if n < 1 {
+		t.Errorf("invalid sending, got sent 0, expected %d\n", len(raw))
+	}
+	time.Sleep(1 * time.Second)
+	Close()
+	w.Wait()
+	// raw = []byte(createArrayRepr([]interface{}{
+	// 	"hello world"
+	// }))
+}
