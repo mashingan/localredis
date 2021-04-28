fe3cbd1b7cc0f7ee2dd16e40e4e85b3336f2c3ff
mashingan <rahmat.d.ruffy@gmail.com>
2021-04-28 20:32:49 +0700

    add getex set test

--- networked_test.go
+++ networked_test.go
@@ -3,6 +3,8 @@
 package localredis
 
 import (
+	"errors"
+	"io"
 	"net"
 	"sync"
 	"testing"
@@ -49,4 +51,58 @@ func TestListenAndServe(t *testing.T) {
 	// raw = []byte(createArrayRepr([]interface{}{
 	// 	"hello world"
 	// }))
+}
+
+func TestGetexSet(t *testing.T) {
+	var w sync.WaitGroup
+	w.Add(1)
+	addr := ":9025"
+	// go ListenAndServe(addr)
+	go func(wg *sync.WaitGroup) {
+		defer w.Done()
+		ListenAndServe(addr)
+	}(&w)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sethello := []interface{}{
+		"set", "hello", "異世界",
+	}
+	nwrite, err := conn.Write([]byte(createReply(sethello)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if nwrite <= 0 {
+		t.Error("failed to write, sent zero bytes")
+	}
+	buff := make([]byte, 128)
+	nread, err := conn.Read(buff)
+	if err != nil && !errors.Is(err, io.EOF) {
+		t.Fatal(err)
+	}
+	if string(buff[:nread]) != "+OK\r\n" {
+		t.Errorf("invalid reply, expected OK, got %s\n", buff[:nread])
+	}
+
+	getarg := createReply([]interface{}{"get", "hello"})
+	t.Log(getarg)
+	nwrite, err = conn.Write([]byte(getarg))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if nwrite <= 0 {
+		t.Error("failed to write, sent zero bytes")
+	}
+	nread, err = conn.Read(buff)
+	if err != nil && !errors.Is(err, io.EOF) {
+		t.Fatal(err)
+	}
+	t.Log("buff:", string(buff))
+	if string(buff[:nread]) != createSimpleString("異世界") {
+		t.Errorf("invalid reply, expected 異世界, got %s\n", buff[:nread])
+	}
+
+	Close()
+	w.Wait()
 }
